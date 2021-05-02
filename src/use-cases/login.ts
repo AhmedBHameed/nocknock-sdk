2e68d6f3492958e6eda0763a10774a5ea5701f35
AhmedBHameed <ahmedbazy@gmail.com>
2021-05-02 03:04:03 +0200

    Imbrase if statement with curlybraces

--- src/use-cases/login.ts
+++ src/use-cases/login.ts
@@ -1,10 +1,11 @@
 import { NockNock } from '../models/NockNockModel'
 
 function login<T = any>(this: NockNock, username: string, password: string): Promise<T> {
-  if (!this._httpClient)
+  if (!this._httpClient) {
     throw new Error(
       'You have to initialize some configuration first. Please call .init() method and set some configuration.'
     )
+  }
 
   return this._httpClient.post<unknown, T>('/nodeys/v1/graphql', {
     query: `
